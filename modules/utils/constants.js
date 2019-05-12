e21fbc1195cbf6e8c44ca4b4025eb5b74b656db5
skobraf <skobraf@gmail.com>
2019-05-12 19:33:54 +0100

    updated procduct

---
+++ modules/utils/constants.js
@@ -0,0 +1,7 @@
+export default {
+  PRODUCT: {
+    GET: 'PRODUCT_GET',
+    GET_SUCCESS: 'PRODUCT_GET_SUCCESS',
+    GET_ERROR: 'PRODUCT_GET_ERROR',
+  },
+};
--- pages/product.js
+++ pages/product.js
@@ -1,1 +1,15 @@
-export default () => <div>Hello! from product page</div>;
+import { connect } from 'react-redux';
+
+const Product = ({ title }) => (
+<div>
+Product:
+{title}
+</div>
+);
+const mapStateToProps = state => ({
+  title: state.product._source.original_title,
+});
+export default Page(connect(mapStateToProps)(Product), async (req, { dispatch }) => {
+  const product = dispatch(fetchProduct(get(req, 'query.id')));
+  await Promise.all([product]);
+});
